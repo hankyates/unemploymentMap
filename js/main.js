a611fb21ae1b68725cf6bb09d7f867db0813675a
Hank Yates <hankyates@gmail.com>
2014-02-05 21:19:09 -0800

    EON commit

--- js/main.js
+++ js/main.js
@@ -9,5 +9,38 @@ require.config({
 });
 
 require(['d3'], function(d3) {
-  console.log(d3);
+  var data; // loaded asynchronously
+
+  var path = d3.geo.path();
+
+  var svg = d3.select("#map")
+  .append("svg:svg");
+
+  var counties = svg.append("svg:g")
+    .attr("id", "counties")
+    .attr("class", "Blues");
+
+  d3.json("../data/us-counties.json", function(json) {
+    counties.selectAll("path")
+    .data(json.features)
+    .enter().append("svg:path")
+    .attr("class", data ? quantize : null)
+    .attr("d", path);
+  });
+
+  d3.json("http://data.wa.gov/resource/ak95-mjh9.json", function (json){
+    data = json;
+    counties.selectAll("path")
+    .attr("class", quantize);
+  });
+
+  //d3.json("../data/unemployment.json", function(json) {
+    //data = json;
+    //counties.selectAll("path")
+    //.attr("class", quantize);
+  //});
+
+  function quantize(d) {
+    return "q" + Math.min(8, ~~(data[d.id] * 9 / 12)) + "-9";
+  }
 });
